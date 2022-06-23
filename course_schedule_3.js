9102d13e762486f4b8085489e9509f7aa51c1745
konstantin56k <konstantin56k@gmail.com>
2022-06-23 15:03:30 -0400

    first commit

---
+++ course_schedule_3.js
@@ -0,0 +1,11 @@
+var scheduleCourse = function(courses) {
+    return courses
+};
+
+let input1 = [[100,200],[200,1300],[1000,1250],[2000,3200]]; //3
+let input2 = [[1,2]]; // 1
+let input3 = [[3,2],[4,3]]; // 0
+
+console.log(scheduleCourse(input1));
+console.log(scheduleCourse(input2));
+console.log(scheduleCourse(input3));
